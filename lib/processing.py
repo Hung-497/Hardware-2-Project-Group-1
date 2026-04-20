0eaa24a0984de1b95bbcf45e19587c23bd44211e
Sorathor <wdqjprvrudwkruq@gmail.com>
2026-04-20 14:07:28 +0300

    add bpm_list for sending to kubios

--- lib/processing.py
+++ lib/processing.py
@@ -16,9 +16,11 @@ class Processing:
         self.sample_count = 0
         self.sma_buffer = []
         self.SMA_WINDOW = 5
+        self.bpm_list = []
 
     def process_sample(self, raw_value):
         # sliding window
+        start = time.ticks_ms()
         self.sma_buffer.append(raw_value)
         if len(self.sma_buffer) > self.SMA_WINDOW:
             self.sma_buffer.pop(0)
@@ -52,14 +54,14 @@ class Processing:
 
             self.beat_intervals.put(interval)
             self.intervals_sum += interval
-
+            
             mean_interval = self.intervals_sum / self.intervals_count
             if mean_interval > 0:
                 calculated_bpm = int(60000 / mean_interval)
                 # check possibility
                 if 40 <= calculated_bpm <= 220:
                     self.bpm = calculated_bpm
-
+                    self.bpm_list.append(self.bpm)    
             self.last_beat_time = now
 
         self.prev_val = value
