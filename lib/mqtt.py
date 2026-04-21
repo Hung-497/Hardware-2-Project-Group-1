f89790a731feaa62a2bbaef57fea2864da9a014b
Sorathor <wdqjprvrudwkruq@gmail.com>
2026-04-21 20:18:37 +0300

    feat: history saving and db payload

--- lib/mqtt.py
+++ lib/mqtt.py
@@ -1,5 +1,6 @@
 import json
 import time
+from storage import Storage
 import network
 import ubinascii
 from umqtt.simple import MQTTClient
@@ -9,9 +10,10 @@ from config import Value
 
 
 class KubiosExample:
-    def __init__(self, bpm_data):
+    def __init__(self, bpm_data, current_bpm=0):
         self.cfg = Value()
         self.bpm_data = bpm_data
+        self.current_bpm = current_bpm
         # This variable will store the latest valid MQTT response
         # that arrives from kubios/response.
         self.latest_response = None
@@ -78,9 +80,27 @@ class KubiosExample:
         with open(filename, "w") as file:
             json.dump(data, file)
 
+    def build_db_payload(self):
+        # publish db payload
+        return {
+            "patient": getattr(self.cfg, "PATIENT_NAME", "Unknown"),
+            "mean_ppi": 800.0,
+            "mean_hr": self.current_bpm,
+            "rmssd": 35.0,
+            "sdnn": 50.0
+        }
+
     def run(self):
         # Step 1: Connect the Pico to Wi-Fi.
         wlan = self.connect_wifi()
+
+        # the NTPtime
+        try:
+            import ntptime
+            ntptime.settime()
+            print("Time synchronized via NTP.")
+        except Exception as e:
+            print("NTP sync failed:", e)
 
         # Step 2: Read the real MAC address of this Pico.
         # This MAC is used in the request and also when checking
@@ -109,6 +129,13 @@ class KubiosExample:
         print(json.dumps(request_payload))
         client.publish(self.cfg.REQUEST_TOPIC, json.dumps(request_payload))
 
+        # publish to database
+        db_topic = getattr(self.cfg, "DB_TOPIC", b"database/hrv")
+        db_payload = self.build_db_payload()
+        if db_payload:
+            client.publish(db_topic, json.dumps(db_payload))
+            print("Published to database:", db_payload)
+
         # Save the current time so we can stop waiting after TIMEOUT_MS.
         start = time.ticks_ms()
 
@@ -125,6 +152,13 @@ class KubiosExample:
                 self.save_json_to_pico(
                     self.cfg.OUTPUT_FILE, self.latest_response)
                 print("Saved to file:", self.cfg.OUTPUT_FILE)
+                
+                # time stamp
+                t = time.localtime()
+                timestamp_str = f"{t[0]:04d}-{t[1]:02d}-{t[2]:02d} {t[3]:02d}:{t[4]:02d}:{t[5]:02d}"
+                Storage(getattr(self.cfg, 'HISTORY_FILE', 'history.json')).save_history(db_payload, self.latest_response, timestamp_str)
+                print("Saved history to:", getattr(self.cfg, 'HISTORY_FILE', 'history.json'))
+
                 break
 
             # Stop waiting if the timeout is exceeded.
