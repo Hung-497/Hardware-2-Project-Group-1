dea90b80b5f6b913cc04d5b0770ab99fc01ae098
Hung-497 <giahung092007@gmail.com>
2026-04-24 23:31:57 +0300

    update collection duration to 30 seconds and remove debug print statement

--- lib/processing.py
+++ lib/processing.py
@@ -137,7 +137,7 @@ class Processing:
             if self.collecting_30s:
                 self.bpm_list.append(interval)
                 # check 30s
-                if time.ticks_diff(now, self.collection_start) >= 10000:
+                if time.ticks_diff(now, self.collection_start) >= 30000:
                     self.collecting_30s = False
                     self.collection_complete = True
 
@@ -145,7 +145,6 @@ class Processing:
                     if len(self.bpm_list) > 0:
                         self.mean_interval = sum(self.bpm_list) / len(self.bpm_list)
                         self.mean_hr = int(60000 / self.mean_interval)
-                        print("Mean HR:", self.mean_hr, "Mean PPI:", self.mean_interval)
                     else:
                         self.mean_hr = 0
                         self.mean_interval = 0
