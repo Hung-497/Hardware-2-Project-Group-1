0a88beef2765d5afb8d7c2f3786271b3e750bf86
Trung <duongmid@metropolia.fi>
2026-04-25 17:05:20 +0300

    add display

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from sampling import Sampling
 from processing import Processing
 from menu import Menu
 from hardware import hw
+from display import Display
 import micropython
+
 
 micropython.alloc_emergency_exception_buf(200)
 
@@ -11,7 +13,8 @@ class Main:
     def main():
         sampler = Sampling()
         processor = Processing()
-        menu = Menu()
+        menu = Menu(processor)
+        disp = Display(processor)
         was_measuring = False
 
         sent = False
@@ -19,8 +22,10 @@ class Main:
         while True:
             # Edge-detect the start of the measurement
             if menu.measuring and not was_measuring:
+                sent = False
                 processor.start_collection()
-            was_measuring = menu.measuring
+                disp.reset()
+            
 
             if not menu.measuring:
                 # clear
@@ -30,27 +35,33 @@ class Main:
                 while not sampler.empty():
                     val = sampler.get()
                     is_beat = processor.process_sample(val)
+                    
+                     # update graph only in basic HR screen
+                    if menu.screen == "basic_hr":
+                        disp.add_sample(processor.prev_val)
 
                     # if heart beats, toggle led
                     if is_beat:
                         hw.led.toggle()
 
-            # Handle the network dispatch directly matched to the menu screen state
-            if getattr(menu, 'screen', '') == "hrv_send":
-                if not sent:
+            # after collecting send to kubios
+            if menu.screen == "hrv_send" and not sent:
+                processor.calculate_hrv_metrics()
+                if menu.menu_option == 4:
+                    print("send to Kubios")
                     try:
                         from mqtt import KubiosExample
                         mqtt_client = KubiosExample(
-                            processor.bpm_list, processor.bpm)
+                            processor.bpm_list, processor.bpm, patient_name=menu.user_manager.get_current_name())
                         mqtt_client.run()
                     except Exception as error:
                         print("kubios failed:", error)
-                    sent = True
-            else:
-                sent = False
+                sent = True
 
             # update measuring screen
-            menu.update_display(processor.bpm)
+            menu.update_display(processor.bpm, disp)
+            
+            was_measuring = menu.measuring
 
 
 Main.main()
